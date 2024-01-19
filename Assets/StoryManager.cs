a74c31e9ef6797bd9ff0302ff09a6ee79b7b5e3f
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-01-20 03:33:22 +0900

    Add StoryManager class with necessary properties and methods

---
+++ Assets/StoryManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryManager : MonoBehaviour
+{
+    [SerializeField]
+    private StoryData[] storyData;
+
+    [SerializeField]
+    private Image background;
+
+    [SerializeField]
+    private TextMeshProUGUI message;
+
+    [SerializeField]
+    private TextMeshProUGUI characterName;
+
+    public int StoryIndex { get; private set; } = 0;
+
+    public int MessageIndex { get; private set; } = 0;
+
+    private void Start()
+    {
+        SetStoryElement(StoryIndex, MessageIndex);
+    }
+
+    private void SetStoryElement(int storyIndex, int messageIndex)
+    {
+        var story = storyData[storyIndex].stories[messageIndex];
+
+        background.sprite = story.Background;
+        message.text = story.Message;
+        characterName.text = story.CharacterName;
+    }
+}
