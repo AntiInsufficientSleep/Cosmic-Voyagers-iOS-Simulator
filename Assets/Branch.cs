73bcf4edf495cd00993fdd81015685d23cb3ed09
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-01-20 16:05:33 +0900

    Add Branch class to project

---
+++ Assets/Branch.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Branch
+{
+    public string choiceMessage;
+    public Chapter chapter;
+}
